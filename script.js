270de4799b0db13aa1e7a9ad141e5f42517653ba
ThanhHoang15 <thanh.hoang210@gmail.com>
2023-04-03 09:47:29 -0700

    initial HTML and JavaScript

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// Creating an array for questions
+var questions = [
+    {
+        question: "What is the largest music festival in the world?"
+        answerA: "Coachella"
+        answerB: "Donauinselfest"
+        answerC: "Summerfest"
+        answerD: "Tomorrowland"
+        correctAnswer: "answerB"
+    },
+
+    {
+        question: "Indio, CA is the home to what music festival?"
+        answerA: "Electric Daisy Carnival/EDC"
+        answerB: "Essense Music Festival"
+        answerC: "Lollapalooza"
+        answerD: "Coachella"
+        correctAnswer: "answerD"
+    }
+
+    {
+        question: "Which is not a music festival promotor company?"
+        answerA: "Live Nation Entertainment"
+        answerB: "AEG Presents"
+        answerC: "LVMH"
+        answerD: "LiveStyle"
+        correctAnswer: "answerC"
+    }
+
+    {
+        question: "Whose are the headlining acts for Coachella 2023?"
+        answerA: "Ariana Grande, Kanye West, Rage Against the Machine"
+        answerB: "Bad Bunny, BLACKPINK, Frank Ocean"
+        answerC: "Harry Styles, Billie Eilish, Swedish House Mafia x The Weeknd"
+        answerD: "Kendrick Lamar, Tame Impala, Coldplay"
+        correctAnswer: "answerB"
+    }
+
+    {
+        question: "Stages at Coachella are named after what?"
+        answerA: "Deserts"
+        answerB: "Mountains"
+        answerC: "Oceans"
+        answerD: "Planets"
+        correctAnswer: "answerA"
+    }
+
+    {
+        question: "So who is trying to buy my Coachella ticket?"
+        answerA: "You"
+        answerB: "You?"
+        answerC: "Youuuu"
+        answerD: "All the above"
+        correctAnswer: "answerD"
+    }
+]
+
